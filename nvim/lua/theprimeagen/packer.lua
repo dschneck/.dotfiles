449a5d08d5d391d430b346230827598847e7ca28
dschneck <david.schneckster@hotmail.com>
2023-03-09 13:20:50 -0500

    removed some stuff and changed other stuff

--- nvim/lua/theprimeagen/packer.lua
+++ nvim/lua/theprimeagen/packer.lua
@@ -33,7 +33,6 @@ return require('packer').startup(function(use)
       end
   })
 
-
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
   use("nvim-treesitter/playground")
   use("theprimeagen/harpoon")
