12163f340e31bfe234960780eb2ad8b381830ca0
dschneck <david.schneckster@hotmail.com>
2023-08-29 09:47:18 -0400

    minor modifications

--- nvim/lua/theprimeagen/remap.lua
+++ nvim/lua/theprimeagen/remap.lua
@@ -2,7 +2,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ov", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w<cr>")
+vim.keymap.set("n", "<leader>W", ":wq<cr>")
 vim.keymap.set("n", "<leader>q", ":q<cr>")
+vim.keymap.set("n", "<leader>Q", ":q!<cr>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- tmux/init.sh
+++ tmux/init.sh
@@ -1,2 +1,8 @@
 rm ~/.tmux.conf
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+alias n='nvim'
+
+rm ~/addCommitPush.sh
+ln -s $(pwd)/addCommitPush ~/addCommitPush.sh
+alias gm='~/addCommitPush.sh'
+chmod +x ~/addCommitPush.sh
